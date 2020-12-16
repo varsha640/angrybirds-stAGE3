9cc19e6f0604083b26512932195e0f9cd43ef1c9
varsha640 <75163044+varsha640@users.noreply.github.com>
2020-12-16 18:44:00 +0530

    Add files via upload

---
+++ angryBirdsStage2/chain.js
@@ -0,0 +1,20 @@
+class Chain {
+    constructor(bodyA,bodyB){
+     var options={
+     bodyA:bodyA,
+     bodyB:bodyB,
+     stiffness :0.10,
+      length:50,
+
+     }
+
+        this.chain=Constraint.create(options)
+        World.add(world,this.chain)
+    }
+    display(){ 
+     var pointA = this.chain.bodyA.position;
+     var pointB = this.chain.bodyB.position;
+
+        line(pointA.x,pointA.y,pointB.x,pointB.y)                 
+    }
+}
